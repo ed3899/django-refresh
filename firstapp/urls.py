6fafab13a8e225bac2d75f6bffb9644663e8eb81
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2025-05-21 15:37:44 +0000

    boom

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('function/', views.hello_world),
-  path('class/', views.HelloEthiopia.as_view())
+  path('class/', views.HelloEthiopia.as_view()),
+  path('reservation/', views.home)
 ]
